b105a37dbd3aed23ce763fde3667e0b162c6c76b
Amirah Nathania <thaniafahreza@gmail.com>
2025-10-12 22:18:06 +0800

    Part 1

--- lib/home.dart
+++ lib/home.dart
@@ -1,21 +1,6 @@
-// Copyright 2018-present the Flutter authors. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'supplemental/asymmetric_view.dart';
-
 import 'colors.dart';
 import 'model/product.dart';
 import 'model/products_repository.dart';
@@ -27,65 +12,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final products = ProductsRepository.loadProducts(category);
-    final formatCurrency = NumberFormat.currency(
-      locale: 'id_ID',
-      symbol: 'Rp',
-    );
-
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: GridView.builder(
-        itemCount: products.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Dua kolom
-          mainAxisSpacing: 16,
-          crossAxisSpacing: 16,
-          childAspectRatio: 8.0 / 9.0,
-        ),
-        itemBuilder: (context, index) {
-          final product = products[index];
-
-          return GestureDetector(
-            onTap: () {
-              Navigator.pushNamed(
-                context,
-                '/product',
-                arguments: product.id,
-              );
-            },
-            child: Card(
-              elevation: 2,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Image.asset(
-                      product.assetName,
-                      package: product.assetPackage,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    product.name,
-                    style: Theme.of(context).textTheme.titleMedium,
-                    maxLines: 1,
-                  ),
-                  Text(
-                    formatCurrency.format(product.price),
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                  const SizedBox(height: 8),
-                ],
-              ),
-            ),
-          );
-        },
-      ),
+    return AsymmetricView(
+      products: ProductsRepository.loadProducts(category),
     );
   }
 }
